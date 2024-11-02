d6600f70cf27faddf85218d873b3d6699e3364e5
joaopespindola <joaopedro.espindola@outlook.com>
2024-11-01 22:55:22 -0300

    feat: Adicionando o trabalho 12

---
+++ trabalho12/ex3-cep-await/conexaoMysql.php
@@ -0,0 +1,23 @@
+<?php
+
+function mysqlConnect()
+{
+  $db_host = "sql313.infinityfree.com";
+  $db_name = "if0_37069041_ppi";
+  $db_username = "if0_37069041";
+  $db_password = "OTU0EAk48caPloO";
+
+  $options = [
+    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ];
+
+  try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password, $options);
+    return $pdo;
+  } 
+  catch (Exception $e) {
+    exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
+  }
+}
+?>
