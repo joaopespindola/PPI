6420962e438154740efc2cbd686b40ec7d285a97
joaopespindola <joaopedro.espindola@outlook.com>
2024-11-01 22:53:51 -0300

    fix: Correção na conexão com o banco

--- trabalho10/conexaoMysql.php
+++ trabalho10/conexaoMysql.php
@@ -3,9 +3,9 @@
 function mysqlConnect()
 {
   $db_host = "sql313.infinityfree.com";
+  $db_name = "if0_37069041_ppi";
   $db_username = "if0_37069041";
   $db_password = "OTU0EAk48caPloO";
-  $db_name = "if0_37069041_ppi";
 
   $options = [
     PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
@@ -15,9 +15,7 @@ function mysqlConnect()
   try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password, $options);
     return $pdo;
-  } 
-  catch (Exception $e) {
+  } catch (Exception $e) {
     exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
   }
 }
-?>
